39b9199b09d6c7ce15d8ff5e7e06e6f44b427504
Jobin <maluplaya08@gmail.com>
2016-06-22 00:04:26 -0500

    22

--- scripts.py
+++ scripts.py
@@ -1,28 +1,71 @@
-Trained model in 0.0035 seconds
-Made predictions in 0.0049 seconds.
-Training a SVC using a training set size of 98. . .
+GaussianNB: 
+
+Training a GaussianNB using a training set size of 100. . .
+Trained model in 0.0032 seconds
+Made predictions in 0.0009 seconds.
+F1 score for training set: 0.8550.
+Made predictions in 0.0007 seconds.
+F1 score for test set: 0.7481.
+--------------------------------
+Training a GaussianNB using a training set size of 200. . .
+Trained model in 0.0025 seconds
+Made predictions in 0.0010 seconds.
+F1 score for training set: 0.8321.
+Made predictions in 0.0008 seconds.
+F1 score for test set: 0.7132.
+--------------------------------
+Training a GaussianNB using a training set size of 300. . .
 Trained model in 0.0027 seconds
-Made predictions in 0.0017 seconds.
-F1 score for training set: 0.8727.
-Made predictions in 0.0050 seconds.
-F1 score for test set: 0.7801.
+Made predictions in 0.0012 seconds.
+F1 score for training set: 0.8088.
+Made predictions in 0.0008 seconds.
+F1 score for test set: 0.7500.
+--------------------------------
 
+DecisionTreeClassifier: 
 
-Trained model in 0.0092 seconds
-Made predictions in 0.0066 seconds.
-Training a SVC using a training set size of 201. . .
-Trained model in 0.0091 seconds
-Made predictions in 0.0066 seconds.
-F1 score for training set: 0.8746.
-Made predictions in 0.0066 seconds.
-F1 score for test set: 0.7742.
+Training a DecisionTreeClassifier using a training set size of 100. . .
+Trained model in 0.0025 seconds
+Made predictions in 0.0003 seconds.
+F1 score for training set: 1.0000.
+Made predictions in 0.0003 seconds.
+F1 score for test set: 0.6942.
+--------------------------------
+Training a DecisionTreeClassifier using a training set size of 200. . .
+Trained model in 0.0046 seconds
+Made predictions in 0.0003 seconds.
+F1 score for training set: 1.0000.
+Made predictions in 0.0003 seconds.
+F1 score for test set: 0.7132.
+--------------------------------
+Training a DecisionTreeClassifier using a training set size of 300. . .
+Trained model in 0.0067 seconds
+Made predictions in 0.0005 seconds.
+F1 score for training set: 1.0000.
+Made predictions in 0.0002 seconds.
+F1 score for test set: 0.7167.
+--------------------------------
 
+SVC: 
 
-Trained model in 0.0193 seconds
-Made predictions in 0.0047 seconds.
+Training a SVC using a training set size of 100. . .
+Trained model in 0.0031 seconds
+Made predictions in 0.0021 seconds.
+F1 score for training set: 0.8591.
+Made predictions in 0.0019 seconds.
+F1 score for test set: 0.7838.
+--------------------------------
+Training a SVC using a training set size of 200. . .
+Trained model in 0.0094 seconds
+Made predictions in 0.0069 seconds.
+F1 score for training set: 0.8693.
+Made predictions in 0.0034 seconds.
+F1 score for test set: 0.7755.
+--------------------------------
 Training a SVC using a training set size of 300. . .
-Trained model in 0.0192 seconds
-Made predictions in 0.0140 seconds.
+Trained model in 0.0193 seconds
+Made predictions in 0.0158 seconds.
 F1 score for training set: 0.8692.
-Made predictions in 0.0049 seconds.
+Made predictions in 0.0047 seconds.
 F1 score for test set: 0.7586.
+--------------------------------
